242d03ae1a0b1238f3fd21d73e558959c89243e2
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-04-03 02:11:26 +0530

    Approach 2 done with successful database connection :)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import express from "express";
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
 
 const app = express();
 
+
+// ----- Connect to MongoDB -----
+import connectDB from "./db/index.js";
+connectDB();
+
+
+
+// ------ dotenv ------
+// require("dotenv").config({path: "./env"});
+
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./env"
+});
+
+
+// ----- Approach 01 -----
+/*
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+
 (async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGODB_HOST}/${DB_NAME}`);
         
         // Handle connection error
         app.on("error", (error) => {
@@ -23,3 +42,5 @@ const app = express();
         console.error(error);
     }
 })();
+
+*/
